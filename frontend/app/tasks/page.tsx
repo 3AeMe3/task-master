0366991b79e60d3d574ad83175b8dca01df8d3f7
3AeMe3 <fm292086@gmail.com>
2026-04-15 17:12:41 -0500

    task page

--- frontend/app/tasks/page.tsx
+++ frontend/app/tasks/page.tsx
@@ -1,7 +1,22 @@
-export default function AllTasks() {
+import TaskClientView from "@/features/task/components/task-client-view";
+import CreateTask from "@/features/task/create-task";
+import GetTask from "@/lib/api/getTasks";
+import { Task } from "@/types/task";
+
+export default async function AllTasks() {
+  const tasks: Task[] = await GetTask();
   return (
-    <div>
-      <h1>AllTasks</h1>
-    </div>
+    <main className="p-10  min-h-screen">
+      <div className="flex justify-between items-center h-30 mb-10">
+        <div>
+          <h1 className="font-semibold text-2xl">AllTasks</h1>
+          <p className="text-black/60">Aqui puedes ver todas tus tareas</p>
+        </div>
+        <CreateTask />
+      </div>
+      <div className=" min-h-screen  flex justify-between gap-3">
+        <TaskClientView taskData={tasks} />
+      </div>
+    </main>
   );
 }
